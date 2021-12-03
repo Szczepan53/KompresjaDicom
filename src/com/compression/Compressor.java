776cbef71848a363fc6e7c7c4d86d781ed8c3f0d
Szczepan53 <radekzelazo@gmail.com>
2021-12-03 19:14:04 +0100

    switched to pixelmed instead of dcm4che2

--- src/com/compression/Compressor.java
+++ src/com/compression/Compressor.java
@@ -1,27 +1,33 @@
 package com.compression;
 
+import com.pixelmed.dicom.AttributeList;
+import com.pixelmed.dicom.DicomException;
+import com.pixelmed.display.SourceImage;
+
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class Compressor {
-    public static String dicomFilePath = "dicom_files\\0015.DCM"; //ścieżka do pliku DICOM, na którym sb testowałem
+    private static String dicomFile = "C:\\Users\\Admin\\IdeaProjects\\PixelmedTest\\0015.DCM";
+    private static String outputJpgFile = "0015compressedJPG.jpg";
+    private static String outputPngFile = "0015compressedPNG.png";
 
 
-    //Testowanko
-    public static void main(String[] args) throws IOException{
-        File dcmFile = new File(dicomFilePath);
-        File jpgFile = new File("compressedImage.jpg");
-        File pngFile = new File("compressedImage.png");
-        compressImage2JPEG(dcmFile, jpgFile, 0.1f);
-        compressImage2PNG(dcmFile, pngFile);
+    public static void main(String[] args) {
+        AttributeList attrList = new AttributeList();
+        try {
+            compressDCM2JPEG(dicomFile, outputJpgFile, 0.05f);
+            compressDCM2PNG(dicomFile, outputPngFile);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /*
@@ -32,49 +38,63 @@ public class Compressor {
 
     Zapisuje obraz pobrany z pliku dicom w formacie definiowanym przez imageWriter.
      */
-    public static void write2Image(BufferedImage image, File compressedImage, ImageWriter imageWriter, ImageWriteParam imageWriteParam) throws IOException {
-        try (ImageOutputStream output = ImageIO.createImageOutputStream(compressedImage)) {
+    public static void write2Image(BufferedImage image, String destPath, ImageWriter imageWriter, ImageWriteParam imageWriteParam) throws IOException {
+        try (ImageOutputStream output = ImageIO.createImageOutputStream(new File(destPath))) {
             imageWriter.setOutput(output);
             IIOImage outputImage = new IIOImage(image, null, null);
             imageWriter.write(null, outputImage, imageWriteParam);
         }
         imageWriter.dispose();
-        System.out.println("Compressed image saved to " + compressedImage.getName());
+        System.out.println("Compressed image saved to " + destPath);
     }
 
 
     /*
-    File originalImage - plik źródłowy DICOM
-    File compressedImage - plik docelowy do zapisu obrazu po kompresji
+    String srcDCMFilePath - ścieżka pliku źródłowego DICOM
+    String destPNGFilePath - ścieżka pliku docelowy do zapisu obrazu po kompresji
     float compressionQuality - wybór stopnia kompresji obrazu w zakresie (0.00f - 1.00f] np. 1.0f = 100% jakość ; 0.2f = 20% jakość itp.
 
     KOMPRESJA STRATNA - JPEG (możliwość wyboru stopnia kompresji poprzez parametr compressionQuality).
-    Przygotowuje imageWriter oraz imageWriterParam dla kompresji obrazu do formatu JPEG.
+    Wczytuje plik DICOM, tworzy BufferedImage na jego podstawie i zapisuje go w formacie JPEG.
      */
-    public static void compressImage2JPEG(File originalImage, File compressedImage, float compressionQuality) throws IOException {
-        BufferedImage image = ImageIO.read(originalImage);
+    public static void compressDCM2JPEG(String srcDCMFilePath, String destJPGFilePath, float compressionQuality)  throws IOException, DicomException {
+        AttributeList attrList = new AttributeList();
+        attrList.read(srcDCMFilePath);
+        SourceImage srcImage = new SourceImage(attrList);
+        BufferedImage bufferedImage = srcImage.getBufferedImage(); //for now only 1 frame to compress
+        compressDCM2JPEG(bufferedImage, destJPGFilePath, compressionQuality);
+    }
+
+    public static void compressDCM2JPEG(BufferedImage bufferedImage, String destJPGFilePath, float compressionQuality) throws IOException {
         ImageWriter jpegWriter = ImageIO.getImageWritersByFormatName("jpg").next();
         ImageWriteParam jpegWriteParam = jpegWriter.getDefaultWriteParam();
         jpegWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
         jpegWriteParam.setCompressionQuality(compressionQuality);
 
-        write2Image(image, compressedImage, jpegWriter, jpegWriteParam);
+        write2Image(bufferedImage, destJPGFilePath, jpegWriter, jpegWriteParam);
 
     }
 
     /*
-    File originalImage - plik źródłowy DICOM
-    File compressedImage - plik docelowy do zapisu obrazu po kompresji
+    String srcDCMFilePath - ścieżka pliku źródłowego DICOM
+    String destPNGFilePath - ścieżka pliku docelowy do zapisu obrazu po kompresji
 
     KOMPRESJA BEZSTRATNA? - PNG (brak możliwości wyboru stopnia kompresji).
-    Przygotowuje imageWriter oraz imageWriterParam dla kompresji obrazu do formatu PNG.
+    Zapisuje obraz wczytany z pliku DICOM w formacie PNG.
      */
-    public static void compressImage2PNG(File originalImage, File compressedImage) throws IOException {
-        BufferedImage image = ImageIO.read(originalImage);
+    public static void compressDCM2PNG(String srcDCMFilePath, String destPNGFilePath) throws IOException, DicomException{
+        AttributeList attrList = new AttributeList();
+        attrList.read(srcDCMFilePath);
+        SourceImage srcImage = new SourceImage(attrList);
+        BufferedImage bufferedImage = srcImage.getBufferedImage(); //for now only 1 frame to compress
+        compressDCM2PNG(bufferedImage, destPNGFilePath);
+    }
+
+    public static void compressDCM2PNG(BufferedImage bufferedImage, String destPNGFilePath) throws IOException{
         ImageWriter pngWriter = ImageIO.getImageWritersByFormatName("png").next();
         ImageWriteParam pngWriteParam = pngWriter.getDefaultWriteParam();
 
-        write2Image(image, compressedImage, pngWriter, pngWriteParam);
+        write2Image(bufferedImage, destPNGFilePath, pngWriter, pngWriteParam);
     }
-}
 
+}
--- src/com/compression/MainFrame.java
+++ src/com/compression/MainFrame.java
@@ -1,2 +1,15 @@
-package com.compression;public class MainFrame {
+package com.compression;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private static Toolbar toolbar;
+
+    public MainFrame(String title) {
+        super(title);
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000, 1000);
+    }
 }
